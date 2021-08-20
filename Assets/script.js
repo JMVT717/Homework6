2cb7f49af559960f3148957b352fc7c23210d361
jmvt717 <jmvt717@josephmilton.world>
2021-08-19 19:34:01 -0500

    uvi set up w lon and lat

--- Assets/script.js
+++ Assets/script.js
@@ -8,35 +8,43 @@ let humEl = document.querySelector(".current-hum")
 let windEl = document.querySelector(".wind-speed")
 let uiEl = document.querySelector(".UVI")
 
+
 fetch(queryUrl)
 .then(headers => headers.json())
 .then(weatherData => {
     console.log(weatherData);
-
+    
     let citName = weatherData.city.name
     let degrees= weatherData.list[0].main.temp;
     let date = weatherData.list[0].dt_txt;
     let citHum = weatherData.list[0].main.humidity;
     let wind = weatherData.list[0].wind.speed;
-    let uvi = weatherData.
-    let lon ;// store values here after an =
-    let lat;// store values here after an =
-
+    let lon = weatherData.city.coord.lon;
+    let lat = weatherData.city.coord.lat;
+    
     cityEl.textContent = citName
     dateEl.textContent = date
     tempElement.textContent = degrees
     humEl.textContent = citHum
     windEl.textContent = wind 
-    uiEl.textContent = uvi
-
-
+    
+    
+    
+    let onecallurl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${apiKey}`
+    console.log(onecallurl)
+    
+    fetch(onecallurl)
+    .then(headers => headers.json())
+    .then(uvdata => {
+        console.log(uvdata);
+        let uvi = uvdata.current.uvi
+        uiEl.textContent = uvi
+    })
     // youre going to need the lat and lon
-
+    
     // you're going to make another fetch request using the lat and lon
     // using a different 
     //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${apiKey}
-    // console.log("lon inside fetch ", lon)
+    
 })
-
-
 
